659006707f06677e1e78ad79eb34f971eef0afdb
gopshyam <gopal.shyam94@gmail.com>
2017-02-05 18:24:51 -0800

    Initial commit

---
+++ client_udp.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <string.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#define SERVER_PORT 5432
+#define MAX_LINE 80
+
+int main(int argc, char * argv[])
+{
+	FILE *fp;
+	struct hostent *hp;
+	struct sockaddr_in sin;
+	char *host;
+	char *fname;
+	char buf[MAX_LINE];
+	char frame[MAX_LINE + 4];
+	int s;
+	int slen;
+	char seq_num = 1;
+	char ack_str[5];
+	int ack_recv;
+	int flen, ack_len;
+
+	struct timeval tv;
+	tv.tv_sec = 1;
+	tv.tv_usec = 0;
+
+	if (argc==3) {
+		host = argv[1];
+		fname= argv[2];
+	}
+	else {
+		fprintf(stderr, "Usage: ./a.out host filename\n");
+		exit(1);
+	}
+	/* translate host name into peer’s IP address */
+	hp = gethostbyname(host);
+	if (!hp) {
+		fprintf(stderr, "Unknown host: %s\n", host);
+		exit(1);
+	}
+
+	fp = fopen(fname, "r");
+	if (fp==NULL){
+		fprintf(stderr, "Can't open file: %s\n", fname);
+		exit(1);
+	}
+
+	/* build address data structure */
+	bzero((char *)&sin, sizeof(sin));
+	sin.sin_family = AF_INET;
+	bcopy(hp->h_addr, (char *)&sin.sin_addr, hp->h_length);
+	sin.sin_port = htons(SERVER_PORT);
+
+	/* active open */
+	if ((s = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
+		perror("Socket");
+		exit(1);
+	}
+	if (setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
+		perror("TIMEOUT ERROR");
+		exit(1);
+	}
+
+	srandom(time(NULL));
+	socklen_t sock_len= sizeof sin;
+
+	
+	/* main loop: get and send lines of text */
+	while(fgets(buf, 80, fp) != NULL){
+		slen = strlen(buf);
+		buf[slen] ='\0';
+
+		frame[0] = seq_num;
+		frame[1] = '\0';
+
+		strcat(frame, buf);
+		flen = strlen(frame);
+		
+		ack_recv = 0;
+		while(!ack_recv) {
+        		if(sendto(s, frame, flen+1, 0, (struct sockaddr *)&sin, sock_len)<0){
+				perror("SendTo Error\n");
+				exit(1);
+			}
+			ack_len = recvfrom(s, ack_str, sizeof(ack_str), 0, (struct sockaddr*)&sin, &sock_len);
+			if (ack_str[0] == seq_num) {
+				ack_recv = 1;
+			}
+		}
+
+	}
+	*buf = 0x02;	
+        if(sendto(s, buf, 1, 0, (struct sockaddr *)&sin, sock_len)<0){
+		perror("SendTo Error\n");
+		exit(1);
+	}
+	fclose(fp);
+}
+
---
+++ server_udp.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <time.h>
+#include <fcntl.h>
+
+#define SERVER_PORT 5432
+#define MAX_LINE 256
+#define SEQ_NO_LEN 5
+
+char parsePacket(char buf[], char payload[]) {
+	char seq_no = buf[0];
+	int framelen, i;
+	framelen = strlen(buf);
+
+	for (i = 2; i < framelen; i++) {
+		payload[i-2] = buf[i];
+	}
+	payload[framelen + 2] = '\0';
+
+
+	return seq_no;
+}
+
+
+int main(int argc, char * argv[])
+{
+	char *fname;
+        char buf[MAX_LINE];
+	char payload[MAX_LINE];
+	char ack_string[5];
+        struct sockaddr_in sin;
+        int len;
+        int s, i;
+        struct timeval tv;
+	char seq_num = 1, highest_seq_num = 0; 
+	FILE *fp;
+
+        if (argc==2) {
+                fname = argv[1];
+        }
+        else {
+                fprintf(stderr, "usage: ./client_udp filename\n");
+        	exit(1);
+        }
+
+
+        /* build address data structure */
+        bzero((char *)&sin, sizeof(sin));
+        sin.sin_family = AF_INET;
+        sin.sin_addr.s_addr = INADDR_ANY;
+        sin.sin_port = htons(SERVER_PORT);
+
+        /* setup passive open */
+        if ((s = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
+                perror("simplex-talk: socket");
+                exit(1);
+        }
+        if ((bind(s, (struct sockaddr *)&sin, sizeof(sin))) < 0) {
+                perror("simplex-talk: bind");
+                exit(1);
+        }
+
+	socklen_t sock_len = sizeof sin;
+	srandom(time(NULL));
+
+	fp = fopen(fname, "w");
+        if (fp==NULL){
+                printf("Can't open file\n");
+                exit(1);
+        }
+	
+	while(1){
+		len = recvfrom(s, buf, sizeof(buf), 0, (struct sockaddr *)&sin, &sock_len);
+		if(len == -1){
+        	    perror("PError");
+		}	
+		else if(len == 1){
+			if (buf[0] == 0x02){
+        	    		printf("Transmission Complete\n");
+				break;
+			}
+        	    	else{
+				perror("Error: Short packet\n");
+			}
+		}	
+		else if(len > 1){
+			seq_num = parsePacket(buf, payload);
+			if (seq_num == highest_seq_num + 1) 
+			{
+				highest_seq_num = seq_num;
+				
+				if(fputs((char *) payload, fp) < 1){
+					printf("fputs() error\n");
+				}
+
+				//Send an acknowledgment
+				ack_string[0] = seq_num;
+				ack_string[1] = '\0';
+				alen = strlen(ack_string);
+				if (sendto(s, ack_string, alen+1, 0, (struct sockaddr *)&sin, sock_len) < 0) {
+					perror("Error in sending an acknowledgement");
+					exit(1);
+				}
+			}
+		}
+
+        }
+	fclose(fp);
+	close(s);
+}
+
+
